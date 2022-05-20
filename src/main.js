b815afb489aaf83b1c76785b6e37c04d7e49c65f
tung1099 <dtroyal1102@gmail.com>
2022-05-20 11:35:08 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+var width;
+var height;
+width = 20.5;
+height = 40;
+var area = width * height;
+console.log("Di\u1EC7n t\u00EDch h\u00ECnh ch\u1EEF nh\u1EADt: ".concat(area));
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+let width: number;
+let height: number;
+
+width = 20.5;
+height = 40;
+
+let area: number = width*height;
+console.log(`Diện tích hình chữ nhật: ${area}`);
